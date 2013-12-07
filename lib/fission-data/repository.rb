322bfe67d6d573d9a563e941f4923674395e6611
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-07 10:42:35 -0800

    Add methods for source namespacing. Include helper for returning short name

--- lib/fission-data/repository.rb
+++ lib/fission-data/repository.rb
@@ -9,9 +9,13 @@ module Fission
 
       value :url, :class => String
       value :name, :class => String
+      value :source, :class => String
+      value :name_source, :class => String
       value :clone_url, :class => String
       value :private
 
+      index :name
+      index :name_source, :unique => true
       index :url, :unique => true
       index :clone_url, :unique => true
 
@@ -19,10 +23,22 @@ module Fission
 
       class << self
 
+        # name:: Repository name
+        # source:: Source of account
+        # Find the given account
+        def lookup(name, source)
+          find_by_name_source(source_key(name, source))
+        end
+
         # List displayable attributes
         def display_attributes
           [:name, :url, :private]
         end
+      end
+
+      # Return short name if long name
+      def short_name
+        name.split('/').last
       end
 
     end
