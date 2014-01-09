ecd491e0d2a1c18195fd807cdc14f66153d21329
Chris Roberts <chrisroberts.code@gmail.com>
2014-01-09 08:19:50 -0800

    Only use name attribute if instance responds to it

--- lib/fission-data/model_base.rb
+++ lib/fission-data/model_base.rb
@@ -324,7 +324,7 @@ module Fission
 
       # Attempt nice string output
       def to_s
-        name || id
+        respond_to?(name) && name ? name : id
       end
 
       # user:: Fission::Data::User instance
