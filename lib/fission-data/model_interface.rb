f8d4ac466a7dc350f384fd9d49f311edd7dd595b
Chris Roberts <chrisroberts.code@gmail.com>
2014-04-13 07:17:50 -0700

    Remove last of local dir globs

--- lib/fission-data/model_interface.rb
+++ lib/fission-data/model_interface.rb
@@ -2,9 +2,9 @@ module Fission
   module Data
     module ModelInterface
 
-      Dir.glob(File.join(File.dirname(__FILE__), 'model_interface', '*')).each do |path|
+      Dir.new(File.join(File.dirname(__FILE__), 'model_interface')).each do |path|
         klass = File.basename(path).sub(File.extname(path), '').split('_').map(&:capitalize).join.to_sym
-        path = "fission-data/model_interface/#{File.basename(path)}"
+        path = "fission-data/model_interface/#{File.basename(path).sub(File.extname(path), '')}"
         autoload klass, path
       end
 
