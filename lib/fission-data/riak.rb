610d272bf86135ee27858247617bd3ac5c6fb35b
Chris Roberts <chrisroberts.code@gmail.com>
2014-04-13 06:46:16 -0700

    Update generated autoload path to provide valid lookups when loading within jar

--- lib/fission-data/riak.rb
+++ lib/fission-data/riak.rb
@@ -36,7 +36,10 @@ module Fission
     end
 
     Dir.glob(File.join(File.dirname(__FILE__), File.basename(__FILE__).sub(File.extname(__FILE__), ''), '*')).map do |file|
-      [File.basename(file).sub(File.extname(file), '').split('_').map(&:capitalize).join.to_sym, file.sub(File.extname(file), '')]
+      [
+        File.basename(file).sub(File.extname(file), '').split('_').map(&:capitalize).join.to_sym,
+        File.join('fission-data/riak', File.basename(file).sub(File.extname(file), ''))
+      ]
     end.uniq.each do |klass_info|
       autoload *klass_info
       Riak.module_eval do
--- lib/fission-data/sql.rb
+++ lib/fission-data/sql.rb
@@ -141,7 +141,10 @@ end
 module Fission
   module Data
     Dir.glob(File.join(File.dirname(__FILE__), File.basename(__FILE__).sub(File.extname(__FILE__), ''), '*')).map do |file|
-      [File.basename(file).sub(File.extname(file), '').split('_').map(&:capitalize).join.to_sym, file.sub(File.extname(file), '')]
+      [
+        File.basename(file).sub(File.extname(file), '').split('_').map(&:capitalize).join.to_sym,
+        File.join('fission-data/sql', File.basename(file).sub(File.extname(file), ''))
+      ]
     end.uniq.each do |klass_info|
       autoload *klass_info
       Sql.module_eval do
