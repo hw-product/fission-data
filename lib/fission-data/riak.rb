596d7b9d0d2eb4c4cbbf3576a576c18a5ca54404
Chris Roberts <chrisroberts.code@gmail.com>
2014-04-13 07:09:12 -0700

    No globing jar local directories

--- lib/fission-data/riak.rb
+++ lib/fission-data/riak.rb
@@ -35,7 +35,7 @@ module Fission
       end
     end
 
-    Dir.glob(File.join(File.dirname(__FILE__), File.basename(__FILE__).sub(File.extname(__FILE__), ''), '*')).map do |file|
+    Dir.new(File.join(File.dirname(__FILE__), File.basename(__FILE__).sub(File.extname(__FILE__), ''))).map do |file|
       [
         File.basename(file).sub(File.extname(file), '').split('_').map(&:capitalize).join.to_sym,
         File.join('fission-data/riak', File.basename(file).sub(File.extname(file), ''))
--- lib/fission-data/sql.rb
+++ lib/fission-data/sql.rb
@@ -140,7 +140,7 @@ end
 
 module Fission
   module Data
-    Dir.glob(File.join(File.dirname(__FILE__), File.basename(__FILE__).sub(File.extname(__FILE__), ''), '*')).map do |file|
+    Dir.new(File.join(File.dirname(__FILE__), File.basename(__FILE__).sub(File.extname(__FILE__)))).map do |file|
       [
         File.basename(file).sub(File.extname(file), '').split('_').map(&:capitalize).join.to_sym,
         File.join('fission-data/sql', File.basename(file).sub(File.extname(file), ''))
