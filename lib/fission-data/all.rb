1615fddfa122f7a65e5f398d2a1906c48fa71f4d
Chris Roberts <chrisroberts.code@gmail.com>
2014-04-29 12:52:23 -0700

    Remove auto loader since it's no longer needed

--- lib/fission-data/all.rb
+++ lib/fission-data/all.rb
@@ -1,12 +1,5 @@
 require 'fission-data'
 
-# Force load constants (generally used for rails)
-Dir.new(File.join(File.dirname(__FILE__), defined?(Fission::Data::Sql) ? 'sql' : 'riak')).each do |path|
-  next if path.start_with?('.')
-  klass = File.basename(path).sub(File.extname(path), '').split('_').map(&:capitalize).join.to_sym
-  Fission::Data.const_get(klass)
-end
-
 # Inject into top level namespace
 if(defined?(Rails))
   Object.send(:include, Fission::Data)
