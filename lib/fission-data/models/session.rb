5fc0bbb1e1e30880b38fe0ec3f565b1e7eb1af81
Chris Roberts <chrisroberts.code@gmail.com>
2014-09-08 12:49:12 -0700

    Force desired data type of internal JSON hash

--- lib/fission-data/models/session.rb
+++ lib/fission-data/models/session.rb
@@ -23,6 +23,14 @@ module Fission
           self.data = Sequel.pg_json(self.data)
         end
 
+        # @return [Smash]
+        def data
+          unless(self.values[:data].is_a?(Smash))
+            self.values[:data] = (self.values[:data] || {}).to_smash
+          end
+          self.values[:data]
+        end
+
       end
 
     end
