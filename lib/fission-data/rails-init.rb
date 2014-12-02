a67107dce49689d929626642dcd0087da0720352
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-02 15:35:37 -0800

    Restrict fission-default-admin user to only admin prefixed paths

--- lib/fission-data/rails-init.rb
+++ lib/fission-data/rails-init.rb
@@ -2,11 +2,11 @@ require 'fission-data'
 
 Rails.application.config.before_initialize do
 
-  unless(Fission::Data::Models::Product.find_by_name('Fission'))
+  unless(Fission::Data::Models::Product.find_by_internal_name('fission'))
     product = Fission::Data::Models::Product.create(:name => 'Fission')
     feature = product.add_product_feature(:name => 'fission_full_access')
     source = Fission::Data::Models::Source.create(:name => 'internal')
-    permission = feature.add_permission(:name => 'fission-admin', :pattern => '.*')
+    permission = feature.add_permission(:name => 'fission-admin', :pattern => 'admin/.*')
     user = source.add_user(:username => 'fission-default-admin')
     user.add_whitelist(:username => 'fission-default-admin')
     user.accounts.first.add_permission(permission)
