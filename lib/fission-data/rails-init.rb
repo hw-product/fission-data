0bc6263605dbce76161aa3a23375a5840eaee0a7
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-04 09:51:58 -0800

    Update rails init on first run to not create whitelist entry and update naming to fission-admin

--- lib/fission-data/rails-init.rb
+++ lib/fission-data/rails-init.rb
@@ -7,8 +7,7 @@ Rails.application.config.before_initialize do
     feature = product.add_product_feature(:name => 'fission_full_access')
     source = Fission::Data::Models::Source.create(:name => 'internal')
     permission = feature.add_permission(:name => 'fission-admin', :pattern => 'admin/.*')
-    user = source.add_user(:username => 'fission-default-admin')
-    user.add_whitelist(:username => 'fission-default-admin')
+    user = source.add_user(:username => 'fission-admin')
     user.accounts.first.add_product_feature(feature)
   end
 
