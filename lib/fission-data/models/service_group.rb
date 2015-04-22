3fa6cbffdaf4b1448e340628346d0a63db5a9a25
Chris Roberts <chrisroberts.code@gmail.com>
2015-04-22 10:19:24 -0700

    Provide override for association creation to allow positioning within join table

--- lib/fission-data/models/service_group.rb
+++ lib/fission-data/models/service_group.rb
@@ -26,6 +26,21 @@ module Fission
           self.services.map(&:name)
         end
 
+        # Association create override to allow positioning
+        #
+        # @param args [Hash]
+        # @option args [Service] :service
+        # @option args [Integer] :position
+        # @return [Array<Service>]
+        def add_service(args)
+          db[:service_groups_services].insert(
+            :service_group_id => self.id,
+            :service_id => args[:service].id,
+            :position => args[:position]
+          )
+          self.reload.services
+        end
+
       end
     end
   end
