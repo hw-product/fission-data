a3c0e32cfeeed97268a021fb6ab8485bbe3b9786
Chris Roberts <code@chrisroberts.org>
2015-05-04 15:56:07 -0700

    Repacking migrations to single init migration. #yolo(?)

--- lib/fission-data/migrations/1394229828_init.rb
+++ lib/fission-data/migrations/1394229828_init.rb
@@ -187,5 +187,110 @@ Sequel.migration do
       index [:path, :product_id], :unique => true
     end
 
+    create_table(:services) do
+      String :name, :null => false, :unique => true
+      String :description
+      DateTime :updated_at
+      DateTime :created_at
+      primary_key :id
+    end
+
+    create_table(:service_groups) do
+      String :name, :null => false, :unique => true
+      String :description
+      DateTime :updated_at
+      DateTime :created_at
+      primary_key :id
+    end
+
+    create_table(:service_groups_services) do
+      Integer :position, :null => false
+      foreign_key :service_group_id, :null => false
+      foreign_key :service_id, :null => false
+      primary_key [:service_group_id, :service_id]
+      index [:service_id, :service_group_id, :position], :unique => true
+    end
+
+    create_table(:product_features_services) do
+      foreign_key :product_feature_id, :null => false
+      foreign_key :service_id, :null => false
+      primary_key [:product_feature_id, :service_id]
+      index [:service_id, :product_feature_id]
+    end
+
+    create_table(:product_features_service_groups) do
+      foreign_key :product_feature_id, :null => false
+      foreign_key :service_group_id, :null => false
+      primary_key [:product_feature_id, :service_group_id]
+      index [:service_group_id, :product_feature_id]
+    end
+
+    create_table(:service_config_items) do
+      String :name, :null => false
+      TrueClass :enabled, :null => false, :default => true
+      String :type, :null => false, :default => 'string'
+      DateTime :updated_at
+      DateTime :created_at
+      primary_key :id
+      foreign_key :service_id, :null => false
+      index [:service_id, :name], :unique => true
+    end
+
+    create_table(:account_configs) do
+      String :name, :null => false
+      String :description
+      column :data, :json
+      DateTime :updated_at
+      DateTime :created_at
+      foreign_key :account_id, :null => false
+      primary_key :id
+      index [:name, :account_id], :unique => true
+    end
+
+    create_table(:routes) do
+      String :name, :null => false
+      String :description
+      DateTime :updated_at
+      DateTime :created_at
+      foreign_key :account_id, :null => false
+      primary_key :id
+    end
+
+    create_table(:routes_services) do
+      foreign_key :route_id, :null => false
+      foreign_key :service_id, :null => false
+      Integer :position, :null => false
+      primary_key [:service_id, :route_id]
+      index [:route_id, :service_id], :unique => true
+      index [:route_id, :service_id, :position], :unique => true
+    end
+
+    create_table(:routes_service_groups) do
+      foreign_key :route_id, :null => false
+      foreign_key :service_group_id, :null => false
+      Integer :position, :null => false
+      primary_key [:service_group_id, :route_id]
+      index [:route_id, :service_group_id], :unique => true
+      index [:route_id, :service_group_id, :position], :unique => true
+    end
+
+    create_table(:custom_services) do
+      String :name, :null => false
+      String :endpoint, :null => false
+      TrueClass :enabled, :null => false, :default => true
+      foreign_key :account_id, :null => false
+      primary_key :id
+      index [:name, :account_id], :unique => true
+    end
+
+    create_table(:custom_services_routes) do
+      foreign_key :route_id, :null => false
+      foreign_key :custom_service_id, :null => false
+      Integer :position, :null => false
+      primary_key [:custom_service_id, :route_id]
+      index [:route_id, :custom_service_id], :unique => true
+      index [:route_id, :custom_service_id, :position], :unique => true
+    end
+
   end
 end
--- lib/fission-data/migrations/1430671133_service_config_options.rb
+++
@@ -1,8 +0,0 @@
-Sequel.migration do
-  change do
-
-    add_column :service_config_items, :enabled, TrueClass, :null => false, :default => true
-    add_column :service_config_items, :type, String, :null => false, :default => 'string'
-
-  end
-end
