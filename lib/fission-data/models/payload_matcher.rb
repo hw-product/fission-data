0473c23967b2f141bfce89171d023ac8834dffd7
Chris Roberts <code@chrisroberts.org>
2015-05-05 08:47:18 -0700

    Add payload matcher and route config models. Include loaders.

---
+++ lib/fission-data/models/payload_matcher.rb
@@ -0,0 +1,27 @@
+require 'fission-data'
+
+module Fission
+  module Data
+    module Models
+
+      # Payload matcher item
+      class PayloadMatcher < Sequel::Model
+
+        many_to_one :payload_match_rule
+        many_to_many :route_configs
+
+        def before_destroy
+          super
+          self.remove_all_route_configs
+        end
+
+        def before_save
+          super
+          validate_presence :value
+        end
+
+      end
+
+    end
+  end
+end
---
+++ lib/fission-data/models/route_config.rb
@@ -0,0 +1,38 @@
+require 'fission-data'
+
+module Fission
+  module Data
+    module Models
+
+      # Configuration for routing
+      class RouteConfig < Sequel::Model
+
+        many_to_one :route
+        many_to_many :account_configs, :order => :position
+        many_to_many :payload_matchers
+
+        def before_destroy
+          super
+          self.remove_all_account_configs
+          self.remove_all_payload_matchers
+        end
+
+        # Association create override to allow positioning
+        #
+        # @param args [Hash]
+        # @option args [AccountConfig] :account_config
+        # @option args [Integer] :position
+        # @return [Array<AccountConfig>]
+        def add_service(args)
+          db[:account_configs_route_configs].insert(
+            :route_config_id => self.id,
+            :account_config_id => args[:account_config].id,
+            :position => args[:position]
+          )
+          self.reload.account_configs
+        end
+
+      end
+    end
+  end
+end
