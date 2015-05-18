c37fa7b9b4d8759a78c36c487816db0ce93fc302
Chris Roberts <code@chrisroberts.org>
2015-05-18 05:46:21 -0700

    Only create source if does not exist

--- lib/fission-data/rails-init.rb
+++ lib/fission-data/rails-init.rb
@@ -2,7 +2,7 @@ require 'fission-data'
 
 Rails.application.config.before_initialize do
 
-  source = Fission::Data::Models::Source.create(:name => 'internal')
+  source = Fission::Data::Models::Source.find_or_create(:name => 'internal')
   Fission::Data::Models::User.find_or_create(
     :username => 'fission-admin',
     :source_id => source.id
