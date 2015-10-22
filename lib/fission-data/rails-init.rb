d0c4a5647d6256455c486d2bc7cc536bae063c84
Chris Roberts <code@chrisroberts.org>
2015-10-22 14:03:01 -0700

    Only setup the callback if capable

--- lib/fission-data/rails-init.rb
+++ lib/fission-data/rails-init.rb
@@ -1,11 +1,13 @@
 require 'fission-data'
 
-Rails.application.config.before_initialize do
+if(defined?(Rails) && Rails.application)
+  Rails.application.config.before_initialize do
 
-  source = Fission::Data::Models::Source.find_or_create(:name => 'internal')
-  Fission::Data::Models::User.find_or_create(
-    :username => 'fission-admin',
-    :source_id => source.id
-  )
+    source = Fission::Data::Models::Source.find_or_create(:name => 'internal')
+    Fission::Data::Models::User.find_or_create(
+      :username => 'fission-admin',
+      :source_id => source.id
+    )
 
+  end
 end
